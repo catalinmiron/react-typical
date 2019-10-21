502918b4f7611836bcfd0efdfdfeb14a8d888aa5
catalinmiron <mironcatalin@gmail.com>
2019-10-21 15:42:13 +0200

    Updates

---
+++ precache-manifest.e5cd2700a208f3db565b84ee0f88cca5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f934c2d735d00c3d2af58801bee764e",
+    "url": "/react-typical/index.html"
+  },
+  {
+    "revision": "8723554466f1d91d2171",
+    "url": "/react-typical/static/css/main.29697137.chunk.css"
+  },
+  {
+    "revision": "aa434f0410d3b33e2280",
+    "url": "/react-typical/static/js/2.cef4ed4a.chunk.js"
+  },
+  {
+    "revision": "8723554466f1d91d2171",
+    "url": "/react-typical/static/js/main.0b010444.chunk.js"
+  },
+  {
+    "revision": "fedf696ceffe4af6e6ef",
+    "url": "/react-typical/static/js/runtime-main.000eebcd.js"
+  }
+]);
